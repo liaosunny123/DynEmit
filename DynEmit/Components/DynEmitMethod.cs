a189ee0de07483054090927b2be76014fb71414a
EpicMo <1982742309@qq.com>
2023-01-25 19:28:02 +0800

    init

---
+++ DynEmit/Components/DynEmitMethod.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection.Emit;
+using DynEmit.Model;
+
+namespace DynEmit.Components;
+
+/// <summary>
+/// 动态方法
+/// </summary>
+public class DynEmitMethod
+{
+    private DynamicMethod _dynEmitMethod;
+
+    public ILGenerator IlGenerator => _il;
+    
+    private ILGenerator _il;
+    /// <summary>
+    /// 构建动态方法
+    /// </summary>
+    /// <param name="name">方法名称</param>
+    /// <param name="returnType">返回类型</param>
+    /// <param name="types">参数列表</param>
+    public DynEmitMethod(string name,Type returnType,List<Type> types)
+    {
+        _dynEmitMethod = new DynamicMethod(name, returnType, types.ToArray());
+        _il = _dynEmitMethod.GetILGenerator();
+    }
+    
+    /// <summary>
+    /// 创建本地 String 变量
+    /// </summary>
+    /// <param name="var"></param>
+    /// <returns></returns>
+    public DynString CreateLocalVariable(string var)
+        => new DynString(_il, var);
+    
+    /// <summary>
+    /// 执行方法，并将结果（如果有）推送到栈顶
+    /// </summary>
+    public void ActionMethod()
+}
---
+++ DynEmit/Model/DynString.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection.Emit;
+
+namespace DynEmit.Model;
+
+/// <summary>
+/// 动态 String
+/// </summary>
+public class DynString
+{
+    private LocalBuilder _var;
+    private ILGenerator _il;
+    public DynString(ILGenerator il,string value)
+    {
+        _il = il;
+        _il.Emit(OpCodes.Ldstr, value);
+        _var = _il.DeclareLocal(typeof(string));
+        _il.Emit(OpCodes.Stloc,_var);
+    }
+
+    public void PushValue() => _il.Emit(OpCodes.Ldstr, _var);
+}
